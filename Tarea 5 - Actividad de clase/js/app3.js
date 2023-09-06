d3490b0e45d8c03c5b93b2c75e374a46a79381fe
Angel Joven <72113587+Angel-Joven@users.noreply.github.com>
2023-09-06 09:24:24 -0600

    Clase del 05 09 2023

---
+++ Tarea 5 - Actividad de clase/js/app3.js
@@ -0,0 +1,36 @@
+// var -> inicializa esa variable y se puede renombrar o sobreescribir
+var nombre = "Angel";
+console.log(nombre);
+
+var nombre = "Cristian";
+console.log(nombre);
+
+// let -> inicializa la variable y no se puede renombrar o sobreescribir (No se puede declarar de nuevo la variable)
+let user = "Angel";
+console.log(user);
+
+//let user = "Cristian";
+//console.log(user);
+
+// const -> Es un "Final" en java, es como un let v2
+
+const nombre2 = "Angel";
+console.log(nombre2);
+
+//const nombre2 = "Cristian";
+//console.log(nombre2);
+
+let numero1 = 10;
+let numero2 = 2;
+
+let suma = numero1 + numero2;
+console.log(suma);
+
+let resta = numero1 - numero2;
+console.log(resta);
+
+let multiplicacion = numero1 * numero2;
+console.log(multiplicacion);
+
+let division = numero1 / numero2;
+console.log(division);
---
+++ Tarea 5 - Actividad de clase/js/app4.js
@@ -0,0 +1,13 @@
+let nombreUser = "Angel";
+let edad = 21;
+let nacionalidad = "Mexicano";
+let comprometido = "No";
+
+console.log("Nombre es: ");
+console.log(nombreUser);
+console.log("Edad es: ");
+console.log(edad);
+console.log("Su nacionalidad es: ");
+console.log(nacionalidad);
+console.log("¿Estas comprometid@? ");
+console.log(comprometido)
