20bc9a18df77c4dbdb9efe1de2ea25eb4ef967e8
Angel Joven <72113587+Angel-Joven@users.noreply.github.com>
2023-11-26 16:22:42 -0600

    Añadido soporte para MySQL Workbench

--- Pokedex_Sistema_de_Informacion/php/conexion.php
+++ Pokedex_Sistema_de_Informacion/php/conexion.php
@@ -10,9 +10,14 @@ SISTEMAS DE INFORMACION | 1758
 
 <?php
 
+// Quite los comentarios dependiendo del 
+// entorno en donde va a trabajar
+
+//PARA XAMPP
+
 // Creamos nuevas variables para poder escribir 
 // ahi nuestras credenciales de MySQL/MariaDB/PHPMyAdmin
-
+/*
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -26,4 +31,17 @@ $conexion_sql = new mysqli($servername, $username, $password, $dbname);
 if ($conexion_sql->connect_error) {
     die("Error de conexión: " . $conexion_sql->connect_error);
 }
+
+*/
+
+//PARA mysql Workbench
+
+$server = 'localhost';
+$user = 'root';
+$pass = '123456789';
+$db =  'pokedex';
+
+$conexion_sql = mysqli_connect($server, $user, $pass, $db)or die("Error en la conexión");
+return $conectar;
+
 ?>
